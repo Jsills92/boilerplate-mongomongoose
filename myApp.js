ba322df90c43605795baf5a809c768571fc27572
Jacob Sills <jsills2592@gmail.com>
2025-03-10 15:47:25 +0000

    Added MongoDB

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
+let mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', () => {
+  console.log("Connected to MongoDB!");
+  // You can add additional logic to verify by performing a query or saving data
+});
 
 let Person;
 
